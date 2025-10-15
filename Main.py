25aeaa0c74f287137793f74f0fb5884d972a0b96
spiderking765 <65326456+spiderking765@users.noreply.github.com>
2025-10-15 11:25:48 -0400

    added .gitignore, for examples being made, should probably organize them into folders

--- Main.py
+++ Main.py
@@ -1,2 +1,2 @@
 ## Initialize libraries, Cyptography for various functions, unsure if it will be handy, will look at documentation
-## importing Tkinter, as the GUI creation tool
+## importing Tkinter, as the GUI creation tool, May try to use the GUI creator
